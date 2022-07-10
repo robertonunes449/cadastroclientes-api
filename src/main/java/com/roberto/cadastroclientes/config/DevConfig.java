05391b28f685112b0f9595b30b483523cc4ab00d
robertonunes449 <roberto.nunes449@gmail.com>
2022-07-10 14:37:10 -0300

    criação perfil DEV

---
+++ src/main/java/com/roberto/cadastroclientes/config/DevConfig.java
@@ -0,0 +1,27 @@
+package com.roberto.cadastroclientes.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.roberto.cadastroclientes.service.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbservice;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	public boolean instanciaBaseDeDAdos() {
+		if(strategy.equals("create")) {
+			this.dbservice.instanciaBaseDeDAdos();
+		}
+		return false;
+	}
+
+}
