c126315d579e98ecfbbb1f73cba7443fc166d253
robertonunes449 <roberto.nunes449@gmail.com>
2022-07-11 18:08:34 -0300

    excecao personalizada para objeto não encontrado

---
+++ src/main/java/com/roberto/cadastroclientes/resource/exceptions/StandardError.java
@@ -0,0 +1,38 @@
+package com.roberto.cadastroclientes.resource.exceptions;
+
+public class StandardError {
+
+	private long timestamp;
+	private Integer status;
+	private String error;
+	public StandardError() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public StandardError(long timestamp, Integer status, String error) {
+		super();
+		this.timestamp = timestamp;
+		this.status = status;
+		this.error = error;
+	}
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+	
+	
+}
--- src/main/java/com/roberto/cadastroclientes/service/ColaboradorService.java
+++ src/main/java/com/roberto/cadastroclientes/service/ColaboradorService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.roberto.cadastroclientes.domain.Colaborador;
 import com.roberto.cadastroclientes.repositories.ColaboradorRepository;
+import com.roberto.cadastroclientes.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class ColaboradorService {
 
 	@Autowired
 	private ColaboradorRepository repository;
-	
+
 	public Colaborador findById(Integer id) {
-		Optional<Colaborador> obj = repository.findById(id); 
-		return obj.orElse(null);	
-		
+		Optional<Colaborador> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id:" + id + ",Tipo:" + Colaborador.class.getName()));
+
 	}
 }
